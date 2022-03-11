e2ef4147687df89d18cfb0a29815f1d78423bb22
Cameron Wong <cw@reosource.io>
2022-03-11 13:36:24 -0800

    Ensure dev dependencies are labeled as such

--- javascript/env.sh
+++ javascript/env.sh
@@ -2,5 +2,5 @@ alias jsbs=' \
     npm init -y && \
     cp ~/.config/cw/dotfile/javascript/.eslint.json .eslintrc.json && \
     cp ~/.config/cw/dotfile/javascript/.jshint.json .jshintrc && \
-    npm install prettier eslint typescript && \
-    npm install eslint-config-prettier jshint'
+    npm install prettier eslint typescript --save-dev && \
+    npm install eslint-config-prettier jshint --save-dev'
