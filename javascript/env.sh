2eb836480ad83ce7b1820c53b35309d3311b03a2
Cameron Wong <cw@reosource.io>
2022-12-01 15:14:22 -1000

    Add jest to default JavaScript environment

--- javascript/env.sh
+++ javascript/env.sh
@@ -3,4 +3,5 @@ alias jsbs=' \
     cp ~/.config/cw/dotfile/javascript/.eslint.json .eslintrc.json && \
     cp ~/.config/cw/dotfile/javascript/.jshint.json .jshintrc && \
     npm install prettier eslint typescript --save-dev && \
-    npm install eslint-config-prettier jshint --save-dev'
+    npm install eslint-config-prettier jshint --save-dev && \
+    npm install eslint-plugin-jest --save-dev'
