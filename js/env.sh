a9c01fee20dc35750a2a67cfd186e5fb0cb7ee1f
Cameron Wong <cw@reosource.io>
2025-11-08 06:06:21 -0800

    Switch to nvm as node/npm tool

--- js/env.sh
+++ js/env.sh
@@ -6,6 +6,6 @@ alias jsbs=' \
     npm install eslint-config-prettier jshint --save-dev && \
     npm install eslint-plugin-jest --save-dev'
 
-if [[ ":$PATH:" != *":${HOME}/opt/npm/bin:"* ]]; then
-    export PATH=$PATH:$HOME/opt/npm/bin
-fi
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
